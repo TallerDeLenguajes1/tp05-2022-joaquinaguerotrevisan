7b3bdd7c95715fe5044c60e61ed126b5c69ea05c
joaquinaguerotrevisan <joaquinaguerotrevisan@gmail.com>
2022-05-25 14:32:43 -0300

    EJ1

---
+++ EJ1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(" === INVERTIR NUMEROS === \n");
+
+string Numero = "", NumeroInvertido = "";
+
+do
+{
+    Console.WriteLine("Escriba el numero a invertir (Mayor que 0): ");
+    Numero = Console.ReadLine();
+
+} while (Numero.Length<=0);
+
+for (int i = Numero.Length; i < 0; i--)
+{
+    NumeroInvertido = NumeroInvertido + Numero[i];
+}
+
+Console.WriteLine("El numero invertido es: {0}",NumeroInvertido);
